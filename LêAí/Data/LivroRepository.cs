457831e8a5cedd095348b904c13972380df05494
Yasmin S. Botelho <yasmin.botelho1@aluno.educa.go.gov.br>
2026-09-21 18:01:17 +0000

    implementação o objeto LIVRO e metodo ADICIONAR

---
+++ LêAí/Data/LivroRepository.cs
@@ -0,0 +1,14 @@
+namespace LêAí.Data
+{
+    public class LivroRepository
+    {
+        privade static List<Livro> _tabelaLivros = new list<Livros>();
+        privade static int proximoID = 1; //Variável auxiliar
+
+        public void Adicionar(Livro livro) //método adiciona
+        {
+            livro.Id = proximoid++;
+            _tabelaLivros.Add(livro);
+        }
+    }
+}
---
+++ LêAí/Models/Livro.cs
@@ -0,0 +1,10 @@
+namespace LêAí.Models
+{
+    public class Livro
+    {
+        public int Id {get; set;}
+        public string Titulo {get; set;}
+        public string Autor {get; set;}
+        public bool Emprestado {get; set;}
+    }
+}
